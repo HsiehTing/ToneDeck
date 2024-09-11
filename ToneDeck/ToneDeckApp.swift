356f42f5b6595395de2cf82f8fcba2cc258029b8
Sting Hsieh <sting20210@gmail.com>
2024-09-11 13:23:50 +0800

    init

---
+++ ToneDeck/ToneDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneDeckApp.swift
+//  ToneDeck
+//
+//  Created by 謝霆 on 2024/9/11.
+//
+
+import SwiftUI
+
+@main
+struct ToneDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
